0eb4acadade6fc71863f0bd8cba79fb0562ea47a
Sibo14 <sibzmbatha870@gmail.com>
2020-12-15 23:53:56 +0200

    Start fast food

--- app/src/main/java/com/vogella/asidle/MainActivity.java
+++ app/src/main/java/com/vogella/asidle/MainActivity.java
@@ -1,14 +1,45 @@
 package com.vogella.asidle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.vogella.asidle.adapter.PopularFoodAdapter;
+import com.vogella.asidle.model.PopularFood;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView popularRecycler;
+    PopularFoodAdapter popularFoodAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<PopularFood> popularFoodList = new ArrayList<>();
+
+        popularFoodList.add(new PopularFood("Pizza", "R85.00",R.drawable.pizza));
+        popularFoodList.add(new PopularFood("Lasagna", "R100.00",R.drawable.lasagna));
+        popularFoodList.add(new PopularFood("Nachos", "R135.00",R.drawable.nachos));
+        popularFoodList.add(new PopularFood("Burger", "R60.00",R.drawable.burger));
+        popularFoodList.add(new PopularFood("Ribs", "R120.00",R.drawable.ribs));
+        popularFoodList.add(new PopularFood("Wings", "R55.00",R.drawable.wings));
+        popularFoodList.add(new PopularFood("Fish & Chips", "R85.00",R.drawable.fish));
+
+        setPopularRecycler(popularFoodList);
+    }
+
+    private void setPopularRecycler(List<PopularFood> popularFoodList){
+
+        popularRecycler = findViewById(R.id.popular_rcycler);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this,RecyclerView.HORIZONTAL,false);
+        popularRecycler.setLayoutManager(layoutManager);
+        popularFoodAdapter = new PopularFoodAdapter(this,popularFoodList);
+        popularRecycler.setAdapter(popularFoodAdapter);
     }
 }
--- app/src/main/java/com/vogella/asidle/adapter/PopularFoodAdapter.java
+++ app/src/main/java/com/vogella/asidle/adapter/PopularFoodAdapter.java
@@ -1,12 +1,16 @@
 package com.vogella.asidle.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.vogella.asidle.R;
 import com.vogella.asidle.model.PopularFood;
 
 import java.util.List;
@@ -24,12 +28,16 @@ public class PopularFoodAdapter extends RecyclerView.Adapter<PopularFoodAdapter.
     @NonNull
     @Override
     public PopularFoodViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.popular_food_row_item,parent,false);
+        return new PopularFoodViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull PopularFoodViewHolder holder, int position) {
 
+        holder.foodImage.setImageResource(popularFoodList.get(position).getImageUrl());
+        holder.name.setText(popularFoodList.get(position).getName());
+        holder.price.setText(popularFoodList.get(position).getPrice());
     }
 
     @Override
@@ -39,8 +47,17 @@ public class PopularFoodAdapter extends RecyclerView.Adapter<PopularFoodAdapter.
 
     public static class PopularFoodViewHolder extends RecyclerView.ViewHolder{
 
+        ImageView foodImage;
+        TextView name,price;
+
         public PopularFoodViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            foodImage = itemView.findViewById(R.id.food_images);
+            name = itemView.findViewById(R.id.name);
+            price = itemView.findViewById(R.id.price);
+
+
         }
     }
 }
